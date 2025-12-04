3c886e27294a4ca02ca086e7b8c4ccfce1ebb456
Raj Kumar Singh <rokerra4144@gmail.com>
2025-12-04 21:46:24 +0530

    first commit

---
+++ project.js
@@ -0,0 +1,16 @@
+console.log("Homepage Loaded Successfully!");
+
+function toggleSidebar() {
+    let sidebar = document.getElementById("sidebar");
+
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+    } else {
+        sidebar.style.width = "250px";
+    }
+}
+
+
+
+
+
