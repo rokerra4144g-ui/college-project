c82d23a6c2ed0529102c6389557db8355d34d7ea
Raj Kumar Singh <rokerra4144@gmail.com>
2025-12-04 22:31:24 +0530

    modified project

--- project.js
+++ project.js
@@ -3,7 +3,7 @@ console.log("Homepage Loaded Successfully!");
 function toggleSidebar() {
     let sidebar = document.getElementById("sidebar");
 
-    if (sidebar.style.width === "250px") {
+    if (sidebar.style.width == "250px") {
         sidebar.style.width = "0";
     } else {
         sidebar.style.width = "250px";
